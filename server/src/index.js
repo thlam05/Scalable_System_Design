c8fbe840c2807be6dd0a1d1cbfb5681fdbab53cc
hlam3004 <hlam.30042005@gmail.com>
2026-04-26 10:31:51 +0700

    add basic api

--- server/src/index.js
+++ server/src/index.js
@@ -1,29 +1,62 @@
 import express from 'express';
 import db from './db/db.js';
+import dotenv from 'dotenv/config';
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-
-    res.send('Hello World!')
-})
+app.use(express.json());
 
 app.get('/health', async (req, res) => {
     try {
         await db.raw('SELECT 1');
 
         res.status(200).json({
-            status: 'UP',
-            database: 'Connected',
-            timestamp: new Date().toISOString()
+            processed_by: `localhost:${process.env.PORT}`,
+            success: true,
         });
     } catch (error) {
-        res.status(503).json({
-            status: 'DOWN',
-            database: 'Disconnected',
-            error: error.message
+        res.json({
+            processed_by: `localhost:${process.env.PORT}`,
+            success: false,
+            message: err.message
         });
+    }
+})
+
+app.get('/products', async (req, res) => {
+    try {
+        const products = await db('products');
+        res.json({
+            processed_by: `localhost:${process.env.PORT}`,
+            success: true,
+            data: products
+        })
+    } catch (err) {
+        res.json({
+            processed_by: `localhost:${process.env.PORT}`,
+            success: false,
+            message: err.message
+        })
+    }
+})
+
+app.post('/products', async (req, res) => {
+    try {
+        const { name, price } = req.body;
+        const [product] = await db('products').insert({ name, price }).returning('*');
+        res.json({
+            processed_by: `localhost:${process.env.PORT}`,
+            success: true,
+            data: product
+        })
+
+    } catch (err) {
+        res.json({
+            processed_by: `localhost:${process.env.PORT}`,
+            success: false,
+            message: err.message
+        })
     }
 })
 
