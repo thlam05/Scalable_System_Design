3fd96b7c71c511900b438893e0db22ec146ff9c1
hlam3004 <hlam.30042005@gmail.com>
2026-04-29 16:18:06 +0700

    update param env

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -4,22 +4,30 @@ import dotenv from 'dotenv/config';
 const masterDB = knex({
     client: "pg",
     connection: {
-        host: process.env.NODE_ENV === 'local' ? 'localhost' : process.env.MASTER_DB_HOST,
-        port: Number(process.env.MASTER_DB_PORT),
-        user: process.env.MASTER_DB_USER,
-        password: process.env.MASTER_DB_PASSWORD,
-        database: process.env.MASTER_DB_DATABASE
+        host: process.env.MASTER_DB_HOST,
+        port: Number(process.env.DB_PORT),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
+    },
+    pool: {
+        min: 2,
+        max: 10
     }
 })
 
 const slaveDB = knex({
     client: "pg",
     connection: {
-        host: process.env.NODE_ENV === 'local' ? 'localhost' : process.env.SLAVE_DB_HOST,
-        port: Number(process.env.SLAVE_DB_PORT),
-        user: process.env.SLAVE_DB_USER,
-        password: process.env.SLAVE_DB_PASSWORD,
-        database: process.env.MASTER_DB_DATABASE
+        host: process.env.SLAVE_DB_HOST,
+        port: Number(process.env.DB_PORT),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
+    },
+    pool: {
+        min: 2,
+        max: 10
     }
 })
 
